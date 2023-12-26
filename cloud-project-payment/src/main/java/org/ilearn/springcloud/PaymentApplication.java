c4f29d218814c413b91eb3b2b5881ca33dfe1bf8
Funeng Wang <fortune_wang95@163.com>
2023-12-26 22:25:57 +0800

    make clusters for payment service

--- cloud-project-payment/src/main/java/org/ilearn/springcloud/PaymentApplication.java
+++ cloud-project-payment/src/main/java/org/ilearn/springcloud/PaymentApplication.java
@@ -2,11 +2,13 @@ package org.ilearn.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentApplication {
     public static void main( String[] args ) {
         SpringApplication.run(PaymentApplication.class,args);
--- cloud-project-payment/src/main/java/org/ilearn/springcloud/controller/PaymentController.java
+++ cloud-project-payment/src/main/java/org/ilearn/springcloud/controller/PaymentController.java
@@ -5,7 +5,12 @@ import org.ilearn.springcloud.entities.Payment;
 import org.ilearn.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/payment")
@@ -14,15 +19,21 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @Value(("${server.port}"))
+    private String serverPort;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @PostMapping("/create")
     public CommonResult<Integer> create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("insert a record :"+result);
 
         if(result>0){
-            return new CommonResult<Integer>(200,"insert succeed.",result);
+            return new CommonResult<Integer>(200,"insert succeed. Server port:"+ serverPort,result);
         }else {
-            return new CommonResult<Integer>(444,"insert fail.",result);
+            return new CommonResult<Integer>(444,"insert fail. Server port:"+serverPort,result);
         }
     }
 
@@ -32,9 +43,22 @@ public class PaymentController {
         log.info("query result: "+payment);
 
         if(payment!=null){
-            return new CommonResult<Payment>(200,"query succeed.",payment);
+            return new CommonResult<Payment>(200,"query succeed.Server port:"+serverPort,payment);
         }else {
-            return new CommonResult<Payment>(444,"Not find record with ID: "+id,null);
+            return new CommonResult<Payment>(444,"Not find record with ID: "+id+"server port: "+serverPort,null);
         }
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service: services){
+            log.info("*******element: "+service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance:instances){
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return discoveryClient;
     }
 }
